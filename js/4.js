ebe3dd7188d979fd3109b7fbf395058c9279a196
Бакотин <bakotin@tmktools.ru>
2023-11-13 09:03:19 +0300

    Добавлено решение 4-й задачи

--- js/4.js
+++ js/4.js
@@ -2,47 +2,27 @@ function main(){
     function Menu(navList, nameList){
         this.navList=navList;
         this.nameList=nameList;
-        this.wrapperA=function() {
-            return this.navList.map(function(elem,i) {
-                console.log(this.nameList); 
+        this.wrapperA=function(){
+            return this.navList.map(function(elem,i){
                 return `<a href='${elem}'>${this.nameList[i]}</a>`;
             }.bind(this));
         }
     }
-
     a = ["home.html","services.html","price.html","about.html"]
     b = ["Главная", "Сервис", "Прайс-лист", "О нас"]
     let mainMenu=new Menu(a,b);
-
     for (elem of mainMenu.navList)
         console.log(elem);
         console.log(mainMenu.wrapperA());
-    // let footerMenu=new Menu("newPage.html","checkPage.html","seoPage.html");
-    // for (elem of footerMenu.navList)
-    //     console.log(elem);
-    //     console.log(footerMenu.wrapperA()); 
+    
+    a = ["newPage.html","checkPage.html","seoPage.html"]
+    b = ["Новая", "Проверка", "Сео"]
+    let footerMenu=new Menu(a,b);
+    for (elem of footerMenu.navList)
+        console.log(elem);
+        console.log(footerMenu.wrapperA()); 
+
 }
-
-
-
-
-// function main(){
-//     function Menu(...navList){
-//         this.navList=navList;
-//         this.wrapperA=function(){
-//             return this.navList.map(elem=>`<a href='${elem}'>item</a>`);
-//         }
-//     }
-//     let mainMenu=new Menu("home.html","services.html","price.html","about.html");
-//     for (elem of mainMenu.navList)
-//         console.log(elem);
-//         console.log(mainMenu.wrapperA());
-//     let footerMenu=new Menu("newPage.html","checkPage.html","seoPage.html");
-//     for (elem of footerMenu.navList)
-//         console.log(elem);
-//         console.log(footerMenu.wrapperA()); 
-
-// }
 
 
 
