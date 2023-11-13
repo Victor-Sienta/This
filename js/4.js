e121ea93902dce2241085dba15d02c3026f38a19
Бакотин <bakotin@tmktools.ru>
2023-11-13 08:30:10 +0300

    up

--- js/4.js
+++ js/4.js
@@ -1,22 +1,45 @@
+// function main(){
+//     function Menu(navList, nameList){
+//         this.navList=navList;
+//         this.nameList=nameList;
+//         this.wrapperA=function(){
+//             return this.navList.map(function(elem,i){
+//                 return `<a href='${elem}'>${this.nameList[i]}</a>`;
+//             });
+//         }
+//     }
+//     a = ["home.html","services.html","price.html","about.html"]
+//     b = ["Главная", "Сервис", "Прайс-лист", "О нас"]
+//     let mainMenu=new Menu(a,b);
+//     for (elem of mainMenu.navList)
+//         console.log(elem);
+//         console.log(mainMenu.wrapperA());
+//     // let footerMenu=new Menu("newPage.html","checkPage.html","seoPage.html");
+//     // for (elem of footerMenu.navList)
+//     //     console.log(elem);
+//     //     console.log(footerMenu.wrapperA()); 
+// }
+
+
+
+
 function main(){
-    function Menu(navList, nameList){
+    function Menu(...navList){
         this.navList=navList;
-        this.nameList=nameList;
         this.wrapperA=function(){
-            return this.navList.map(function(elem,i){
-                return `<a href='${elem}'>${this.nameList[i]}</a>`;
-            });
+            return this.navList.map(elem=>`<a href='${elem}'>item</a>`);
         }
     }
-    a = ["home.html","services.html","price.html","about.html"]
-    b = ["Главная", "Сервис", "Прайс-лист", "О нас"]
-    let mainMenu=new Menu(a,b);
+    let mainMenu=new Menu("home.html","services.html","price.html","about.html");
     for (elem of mainMenu.navList)
         console.log(elem);
         console.log(mainMenu.wrapperA());
-    // let footerMenu=new Menu("newPage.html","checkPage.html","seoPage.html");
-    // for (elem of footerMenu.navList)
-    //     console.log(elem);
-    //     console.log(footerMenu.wrapperA()); 
+    let footerMenu=new Menu("newPage.html","checkPage.html","seoPage.html");
+    for (elem of footerMenu.navList)
+        console.log(elem);
+        console.log(footerMenu.wrapperA()); 
 
 }
+
+
+
