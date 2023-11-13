f73f3e338ad1309877c8c2e78bf93718fee3db02
Бакотин <bakotin@tmktools.ru>
2023-11-13 08:34:03 +0300

    +

--- js/4.js
+++ js/4.js
@@ -1,45 +1,47 @@
-// function main(){
-//     function Menu(navList, nameList){
-//         this.navList=navList;
-//         this.nameList=nameList;
-//         this.wrapperA=function(){
-//             return this.navList.map(function(elem,i){
-//                 return `<a href='${elem}'>${this.nameList[i]}</a>`;
-//             });
-//         }
-//     }
-//     a = ["home.html","services.html","price.html","about.html"]
-//     b = ["Главная", "Сервис", "Прайс-лист", "О нас"]
-//     let mainMenu=new Menu(a,b);
-//     for (elem of mainMenu.navList)
-//         console.log(elem);
-//         console.log(mainMenu.wrapperA());
-//     // let footerMenu=new Menu("newPage.html","checkPage.html","seoPage.html");
-//     // for (elem of footerMenu.navList)
-//     //     console.log(elem);
-//     //     console.log(footerMenu.wrapperA()); 
-// }
-
-
-
-
 function main(){
-    function Menu(...navList){
+    function Menu(navList, nameList){
         this.navList=navList;
-        this.wrapperA=function(){
-            return this.navList.map(elem=>`<a href='${elem}'>item</a>`);
+        this.nameList=nameList;
+        this.wrapperA=function() {
+            return this.navList.map(function(elem,i){
+                return `<a href='${elem}'>${this.nameList[i]}</a>`;
+            });
         }
     }
-    let mainMenu=new Menu("home.html","services.html","price.html","about.html");
+
+    a = ["home.html","services.html","price.html","about.html"]
+    b = ["Главная", "Сервис", "Прайс-лист", "О нас"]
+    let mainMenu=new Menu(a,b);
+
     for (elem of mainMenu.navList)
         console.log(elem);
         console.log(mainMenu.wrapperA());
-    let footerMenu=new Menu("newPage.html","checkPage.html","seoPage.html");
-    for (elem of footerMenu.navList)
-        console.log(elem);
-        console.log(footerMenu.wrapperA()); 
-
+    // let footerMenu=new Menu("newPage.html","checkPage.html","seoPage.html");
+    // for (elem of footerMenu.navList)
+    //     console.log(elem);
+    //     console.log(footerMenu.wrapperA()); 
 }
+
+
+
+
+// function main(){
+//     function Menu(...navList){
+//         this.navList=navList;
+//         this.wrapperA=function(){
+//             return this.navList.map(elem=>`<a href='${elem}'>item</a>`);
+//         }
+//     }
+//     let mainMenu=new Menu("home.html","services.html","price.html","about.html");
+//     for (elem of mainMenu.navList)
+//         console.log(elem);
+//         console.log(mainMenu.wrapperA());
+//     let footerMenu=new Menu("newPage.html","checkPage.html","seoPage.html");
+//     for (elem of footerMenu.navList)
+//         console.log(elem);
+//         console.log(footerMenu.wrapperA()); 
+
+// }
 
 
 
