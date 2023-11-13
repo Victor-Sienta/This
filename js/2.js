3dadc7031405a15bf05fe2c4d1fe8037d4b23bdc
Бакотин <bakotin@tmktools.ru>
2023-11-13 08:17:34 +0300

    update

--- js/2.js
+++ js/2.js
@@ -1,27 +1,30 @@
-let orders=[
-    {
-        fio:"Ivan",
-        tovar:"phone",
-        price:10000,
-        quantity:2
-    },
-    {
-        fio:"Peter",
-        tovar:"usb",
-        price:2300,
-        quantity:5
-    },
-    {
-        fio:"Mary",
-        tovar:"connector",
-        price:100,
-        quantity:100
-    },
-    ]
+function main(){
+        let orders=[
+        {
+            fio:"Ivan",
+            tovar:"phone",
+            price:10000,
+            quantity:2
+        },
+        {
+            fio:"Peter",
+            tovar:"usb",
+            price:2300,
+            quantity:5
+        },
+        {
+            fio:"Mary",
+            tovar:"connector",
+            price:100,
+            quantity:100
+        },
+        ]
 
-    function showOrder(){
-        let str = (`Заказ покупателя ${this.fio} составил ${this.price*this.quantity}. Товар ${this.tovar}`);
-        console.log(str);
-    }
+        function showOrder(){
+            let str = (`Заказ покупателя ${this.fio} составил ${this.price*this.quantity}. Товар ${this.tovar}`);
+            console.log(str);
+        }
 
-    orders.forEach(elem=>showOrder.call(elem));
+        orders.forEach(elem=>showOrder.call(elem));
+
+}
