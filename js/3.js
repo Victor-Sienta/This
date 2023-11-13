6505ff6a3329874d93d8626b6eda28f4bc8fa8b6
Бакотин <bakotin@tmktools.ru>
2023-11-13 10:13:36 +0300

    Обвноление задания 2

--- js/3.js
+++ js/3.js
@@ -5,29 +5,26 @@ function main(){
         tovar:"phone",
         price:10000,
         quantity:2,
-        discount:20
     },
     {
         fio:"Peter",
         tovar:"usb",
         price:2300,
         quantity:5,
-        discount:10
     },
     {
         fio:"Mary",
         tovar:"connector",
         price:100,
         quantity:100,
-        discount:5
     },
     ]
 
-    function showOrder(){
-        let str = (`Заказ покупателя ${this.fio} составил ${this.price*this.quantity*((100-this.discount)*0.01)} Товар ${this.tovar}. Скидка составила ${this.discount}%`);
+    function showOrder(discount){
+        let str = (`Заказ покупателя ${this.fio} составил ${this.price*this.quantity*((100-discount)/100)} Товар ${this.tovar}. Скидка составила ${discount}%`);
         console.log(str);
     }
 
-    orders.forEach(elem=>showOrder.call(elem));
+    orders.forEach(elem=>showOrder.call(elem,10));
 
 }
